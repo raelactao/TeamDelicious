9025aa823f6f672152db1dd5918f4ff9a99d3eab
raelactao <raevictoria.lactao@benilde.edu.ph>
2022-10-17 17:41:45 +0800

    Created

---
+++ 1t/src/main/java/com/teamdelicious/appadvc2223/t/TeamDeliciousApplication.java
@@ -0,0 +1,13 @@
+package com.teamdelicious.appadvc2223.t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDeliciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDeliciousApplication.class, args);
+	}
+
+}
