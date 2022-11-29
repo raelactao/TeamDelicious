29730dd5b44b1c1c5bfd868faa076fb420e4ca86
raelactao <raevictoria.lactao@benilde.edu.ph>
2022-11-29 08:44:19 +0800

    moved files to labelled folders

---
+++ 1t/src/main/java/com/teamdelicious/appadvc2223/t/controllers/ReservationController.java
@@ -0,0 +1,11 @@
+package com.teamdelicious.appadvc2223.t.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reservations")
+public class ReservationController {
+
+
+}
---
+++ 1t/src/main/java/com/teamdelicious/appadvc2223/t/dto/ReservationDTO.java
@@ -0,0 +1,86 @@
+package com.teamdelicious.appadvc2223.t.dto;
+
+import com.teamdelicious.appadvc2223.t.model.Reservation;
+
+public class ReservationDTO {
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String contactNumber;
+
+    private String emailAddress;
+
+    private String paxNumber;
+
+    public ReservationDTO() {}
+
+    public ReservationDTO(Long id, String firstName, String lastName, String contactNumber, String emailAddress, String paxNumber) {
+        this.id = id;
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setContactNumber(contactNumber);
+        this.setEmailAddress(emailAddress);
+        this.setPaxNumber(paxNumber);
+    }
+
+
+
+    public ReservationDTO(Reservation reservation) {
+        this.setId(reservation.getId());
+        this.setFirstName(reservation.getFirstName());
+        this.setLastName(reservation.getLastName());
+        this.setContactNumber(reservation.getContactNumber());
+        this.setPaxNumber(reservation.getPaxNumber());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPaxNumber() {
+        return paxNumber;
+    }
+
+    public void setPaxNumber(String paxNumber) {
+        this.paxNumber = paxNumber;
+    }
+}
