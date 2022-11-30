25ac1654601bca606e0d5e9e474f8cb7310c0928
svq49 <simon.que@benilde.edu.ph>
2022-11-30 14:05:39 +0800

    application changes

--- 1t/src/main/java/com/teamdelicious/appadvc2223/t/security/SecurityConfiguration.java
+++ 1t/src/main/java/com/teamdelicious/appadvc2223/t/security/SecurityConfiguration.java
@@ -5,18 +5,51 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfiguration implements WebMvcConfigurer {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    //@Autowired
-    //CustomAuthenticationProvider customAuthenticationProvider;
+    @Autowired
+    CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
+    }
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","/login*","/register")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home",true)
+                .usernameParameter("emailAddress")
+                .and()
+                .logout()
+                .permitAll();
+    }
 
     //@Autowired
     //PasswordEncoder passwordEncoder;
-/*
+
+    /*
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -37,6 +70,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
         return http.build();
     }
+    */
 
 
     /*
@@ -44,7 +78,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     protected void configure(final AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
     }
+*/
 
-    */
 
 }
