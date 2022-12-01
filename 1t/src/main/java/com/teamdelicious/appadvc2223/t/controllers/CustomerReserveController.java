2ce8e2edef3313942b851005a3e80f52ebf9a1a8
raelactao <raevictoria.lactao@benilde.edu.ph>
2022-12-01 11:02:19 +0800

    added successfull validation to reservation

--- 1t/src/main/java/com/teamdelicious/appadvc2223/t/controllers/CustomerReserveController.java
+++ 1t/src/main/java/com/teamdelicious/appadvc2223/t/controllers/CustomerReserveController.java
@@ -5,8 +5,13 @@ import com.teamdelicious.appadvc2223.t.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller("reserve/customer")
 public class CustomerReserveController {
@@ -16,19 +21,24 @@ public class CustomerReserveController {
     @Autowired
     private ReservationService reservationService;
 
-    @GetMapping("/")
-    private String getHome(Model model) {
-        return "index";
-    }
 
-    @GetMapping("create")
+    @GetMapping()
     private String getReservationAddForm(Model model) {
         model.addAttribute("reservation", new ReservationDTO());
         return "reserve/customer/create";
     }
 
     @PostMapping
-    private String addReservation(ReservationDTO reservationDTO, Model model) {
+    private String addReservation(@Valid @ModelAttribute("reservation") ReservationDTO reservationDTO,
+                                  BindingResult result, Model model) {
+        String err = reservationService.validateReservedDateTime(reservationDTO);
+        if (!err.isEmpty()) {
+            ObjectError error = new ObjectError("globalError", err);
+            result.addError(error);
+        }
+        if (result.hasErrors()) {
+            return "reserve/customer/create";
+        }
         reservationService.add(reservationDTO);
         return "reserve/customer/create";
     }
